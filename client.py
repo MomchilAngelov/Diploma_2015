e22ddd9188c1323121154c6915ead02cbacc7fe3
momodota2@gmail.com <momodota2@gmail.com>
2016-06-01 07:11:35 +0300

    Uprint("Please input the data:") 		input_str = input() 		input_str = str.encode(input_str) pdating!

--- client.py
+++ client.py
@@ -3,15 +3,17 @@ import sys
 import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-port_num = int(sys.argv[1])
-
 
 try:
 	while True:
+		print("Please input the port:")
+		input_port = input()
+		input_port = int(input_port)
 
+		print("Please input the data:")
 		input_str = input()
 		input_str = str.encode(input_str)
 
-		sock.sendto(input_str,('localhost', port_num))
+		sock.sendto(input_str,('localhost', input_port))
 finally:
 	sock.close()
